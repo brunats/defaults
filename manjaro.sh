3b5fe30df61707f7617ad2e518b3ff19b3d3b7be
Marlon Henry Schweigert <marlon.henry@magrathealabs.com>
2018-12-11 22:26:23 -0200

    Install rvm

--- manjaro.sh
+++ manjaro.sh
@@ -13,3 +13,7 @@ sudo snap install atom --classic
 
 # Generate SSH Key for github
 ssh-keygen
+
+# RVM
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable
