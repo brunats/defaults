6df5661be2c9439c2aabc1a7a8eba206be9082bb
Bruna <silvatavares.bruna@gmail.com>
2019-03-15 14:45:40 -0300

    Install Python 2 and 3

--- mac.sh
+++ mac.sh
@@ -1,6 +1,17 @@
-# Instalar o brew
+# Instalar o homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 
 # Instalar o Atom
 brew cask install atom
+
+# Instalar o XCode
+xcode-select --install
+
+# Instalar e Atualizar pip
+sudo easy_install pip
+sudo pip install --upgrade pip
+
+# Python 2 e 3
+brew install python
+brew install python3
