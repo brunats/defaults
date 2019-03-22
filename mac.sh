510574d636c4d5848ce8290a9951ce46c15c53d7
Bruna <silvatavares.bruna@gmail.com>
2019-03-22 11:34:40 -0300

    Fix error not import zlib

--- mac.sh
+++ mac.sh
@@ -12,6 +12,12 @@ xcode-select --install
 sudo easy_install pip
 sudo pip install --upgrade pip
 
+# Zlib
+brew install zlib
+export LDFLAGS="${LDFLAGS} -L/usr/local/opt/zlib/lib"
+export CPPFLAGS="${CPPFLAGS} -I/usr/local/opt/zlib/include"
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH} /usr/local/opt/zlib/lib/pkgconfig"
+
 # Python 2 e 3
 brew install python
 brew install python3
