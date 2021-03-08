1e81b637f31427f53e3331cf0c268c738de3bec6
Bruna Tavares Silva <silvatavares.bruna@gmail.com>
2021-03-08 15:23:00 -0300

    Add default to ubuntu

---
+++ ubuntu.sh
@@ -0,0 +1,43 @@
+# Essential
+sudo apt-get install build-essential
+sudo apt install curl
+
+# VScode
+sudo snap install --classic code
+
+# Zsh
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Git
+sudo apt install git
+git config --global user.name "USER_NAME"
+git config --global user.email "USER_EMAIL"
+git config --global user.signingkey "USER_SIGNINGKEY"
+
+# Docker
+sudo apt-get install docker
+sudo apt-get install docker-compose
+
+# ASDF
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
+
+## Golang
+asdf plugin add golang
+asdf install golang latest
+asdf list nodejs
+asdf global golang 1.14
+
+## NodeJs
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
+asdf install nodejs latest
+asdf list nodejs
+asdf global 15.11.0
+
+# Spotify Slack Discord
+sudo snap install spotify
+sudo snap install slack --classic
+sudo snap install discord
+
+
